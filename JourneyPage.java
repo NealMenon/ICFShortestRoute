23b29c9ab5ccf5f3fc81d737dcb6ef2774cce27b
aayush-v <51158170+aayush-v@users.noreply.github.com>
2019-06-11 14:06:01 +0530

    Journey Result

---
+++ JourneyPage.java
@@ -0,0 +1,59 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+
+public class JourneyPage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JourneyPage window = new JourneyPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JourneyPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBack.setBounds(172, 215, 117, 29);
+		frame.getContentPane().add(btnBack);
+		
+		JLabel lblTheJourneyIs = new JLabel("The journey is as follows");
+		lblTheJourneyIs.setBounds(153, 27, 202, 16);
+		frame.getContentPane().add(lblTheJourneyIs);
+	}
+}
